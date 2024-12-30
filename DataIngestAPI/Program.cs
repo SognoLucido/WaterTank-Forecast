6cb9b496b0b4545408cb64b2ae49349091dae2ef
Francesco Barbano <francesco.barbano96@gmail.com>
2024-12-30 18:32:27 +0100

    base setup

---
+++ DataIngestAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataIngestAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
